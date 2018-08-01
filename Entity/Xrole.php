a346465f084133487f31f4935487a2969d337408
HETRU Mathieu <mathieu.hetru@laposte.net>
2018-08-01 22:21:41 +0200

    modif

---
+++ Entity/Xrole.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace L3\Bundle\DbUserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role;
+
+/**
+ * @ORM\Table(name="x_role")
+ * @ORM\Entity(repositoryClass="L3\Bundle\DbUserBundle\Repository\RoleRepository")
+ */
+class Xrole extends Role {
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="role", type="string", length=255)
+     *
+     * @var string $role
+     */
+    private $role;
+
+
+    public function __toString() {
+        return $this->getRole();
+    }
+
+    /**
+     * Get the id.
+     *
+     * @return integer The id.
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * Set id
+     *
+     * @param string $id
+     * @return Role
+     */
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+
+    /**
+     * Implementation of getRole for the RoleInterface.
+     *
+     * @return string The role.
+     */
+    public function getRole() {
+        return $this->role;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return Role
+     */
+    public function setRole($role) {
+        $this->role = $role;
+        return $this;
+    }
+
+}
