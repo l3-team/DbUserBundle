d8734624c9cd66ecb3c6723c69f8ef81a6ee2863
Hetru Mathieu <mathieu.hetru@univ-lille.fr>
2023-12-31 13:37:23 +0100

    modif for sf7.0

--- Entity/User.php
+++ Entity/User.php
@@ -6,34 +6,22 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 
-
-/**
- * @ORM\Table(name="x_user")
- * @ORM\Entity(repositoryClass="L3\Bundle\DbUserBundle\Repository\UserRepository")
- */
+#[ORM\Table(name:"x_user")]
+#[ORM\Entity(repositoryClass:"L3\Bundle\DbUserBundle\Repository\UserRepository")]
 class User implements UserInterface, \Serializable {
 
-    /**
-     * @var integer $id
-     * @ORM\Id
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy:"AUTO")]
+    #[ORM\Column(name:"id", type:"integer")]
     private $id;
 
-    /**
-     * @ORM\Column(name="uid", type="string", length=15)
-     */
+    #[ORM\Column(name:"uid", type:"string", length:15)]
     private $uid;
-
-    /**
-     * @ORM\ManyToMany(targetEntity="Xrole", cascade={"persist"})
-     * @ORM\JoinTable(name="x_user_role",
-     *     joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="role_id", referencedColumnName="id")}
-     * )
-     * @var ArrayCollection $roles
-     */
+    
+    #[ORM\ManyToMany(targetEntity:"Xrole")]
+    #[ORM\JoinTable(name:"x_user_role")]
+    #[ORM\JoinColumn(name:"user_id", referencedColumnName:"id")]
+    #[ORM\InverseJoinColumn(name:"role_id", referencedColumnName:"id")]
     private $roles;
 
     
